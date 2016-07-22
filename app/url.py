22369d410a88041294cc561ed2cb1925cec775ed
orekinana <zmnn.sherry@icloud.com>
2016-07-22 15:04:50 +0800

    compelete get_grade method

--- app/url.py
+++ app/url.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from ctrl.grade import GradeCtrl
-from ctrl.
+
 
 url = [
     (r'/grade', GradeCtrl),
-    (r'/table', TableCtrl),
-    (r'/library', LibraryCtrl),
-    (r'/card', CardCtrl),
-    (r'/net', NetCtrl),
-    (r'/news', NewsCtrl),
+    # (r'/table', TableCtrl),
+    # (r'/library', LibraryCtrl),
+    # (r'/card', CardCtrl),
+    # (r'/net', NetCtrl),
+    # (r'/news', NewsCtrl),
 ]
