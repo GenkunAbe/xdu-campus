46807fe2cb0f69344acee797af32c187bda5fabf
orekinana <zmnn.sherry@icloud.com>
2016-07-29 23:55:07 +0800

    add get_table method

--- app/ctrl/table.py
+++ app/ctrl/table.py
@@ -2,8 +2,15 @@
 
 import tornado.web
 import sys
+import json
 sys.path.append('..')
+from model.jwxt import Jwxt
 
 class TableCtrl(tornado.web.RequestHandler):
     def get(self):
-        self.write('Please Wait!\n')
+        username = self.get_argument('username', '123456789')
+        password = self.get_argument('password', '888888')
+        jwxt = Jwxt()
+        table = jwxt.get_table(username, password, '2016-2017', '1', '1403018')
+        table_json = json.dumps(table)
+        self.write(table_json)
